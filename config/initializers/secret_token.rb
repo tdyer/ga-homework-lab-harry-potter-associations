9e1f09c9ac4a882f6dddcbc70ca14b9b4984e9bd
Tom Dyer <tdyer1@gmail.com>
2013-10-17 17:25:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GaHomeworkLabHarryPotterAssociations::Application.config.secret_key_base = '92c4b72d11b9b815ea5d98d90e8172adb7fc83946b7b68c482e9a720f462f9fca16265b9677dddc330365d4c4d93cbbab1012c51f1532dfaaa390c860c508e65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GaHomeworkLabHarryPotterAssociations::Application.config.session_store :cookie_store, key: '_ga-homework-lab-harry-potter-associations_session'
